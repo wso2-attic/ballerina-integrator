110873b2cd204b1059c80114fb0e2c5e175e04d5
Kasun Indrasiri <kasun147@gmail.com>
2018-03-22 13:43:10 -0600

    Add sample bal file

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # declare STRING variable
 STRING="Downloading ballerina..."
-BALLERINA_VERSION="ballerina-0.964.0"
+BALLERINA_VERSION="ballerina-tools-0.964.0"
 #print variable on a screen
 echo $STRING
 #download ballerina distro
-wget https://ballerinalang.org/downloads/ballerina-runtime/$BALLERINA_VERSION.zip
+#wget https://ballerinalang.org/downloads/ballerina-runtime/$BALLERINA_VERSION.zip
+wget https://ballerinalang.org/downloads/ballerina-tools/$BALLERINA_VERSION.zip
 #unzip the zip file
 unzip $BALLERINA_VERSION.zip
 
