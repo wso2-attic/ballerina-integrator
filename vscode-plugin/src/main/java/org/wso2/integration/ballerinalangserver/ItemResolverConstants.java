c505ed1508231c8efa096b9a67e17f7b5dd0e8a0
nipunich <nipu6666@gmail.com>
2019-08-12 11:03:16 +0530

    Implement Ballerina Integrator VSCode plugin snippet generation capability.

---
+++ vscode-plugin/src/main/java/org/wso2/integration/ballerinalangserver/ItemResolverConstants.java
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+//
+// WSO2 Inc. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.wso2.integration.ballerinalangserver;
+
+public class ItemResolverConstants {
+
+    // Start of Basic Constructs
+    public static final String SERVICE = "service/http";
+    public static final String SERVICE_WEBSOCKET = "service/webSocket";;
+    public static final String SERVICE_GRPC = "service/gRPC";
+    public static final String RESOURCE = "resource";
+    // End of Basic Constructs
+
+    // Symbol Types Constants
+    public static final String SNIPPET_TYPE = "Snippet";
+    public static final String RECORD_TYPE = "type <RecordName> record";
+    // End Symbol Types Constants
+}
