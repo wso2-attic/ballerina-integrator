da61fe9bee4d7e6b51a4aaa144df32a00ccfb9d2
Dinuksha <dinuksha.ishwari@gmail.com>
2019-09-30 16:58:35 +0530

    Remove service composition from test script

--- docs/content/resources/run.sh
+++ docs/content/resources/run.sh
@@ -48,15 +48,14 @@ execute_tests() {
     "message-filtering" "pass-through-messaging" "scatter-gather-messaging-advanced" "asynchronous-invocation"
     "asynchronous-invocation" "parallel-service-orchestration" "parallel-service-orchestration"
     "parallel-service-orchestration" "parallel-service-orchestration" "exposing-a-rest-service"
-    "service-composition" "service-composition" "service-composition" "writing-tests-using-data-providers")
+    "writing-tests-using-data-providers")
     executionPathList=(${path1} ${path2} ${path3} ${path4} ${path5} ${path5} ${path5} ${path5} ${path5} ${path5} ${path5} ${path6}
-    ${path7} ${path8} ${path9} ${path10} ${path10} ${path11} ${path11} ${path11} ${path11} ${path12} ${path14} ${path14}
-    ${path14} ${path15})
+    ${path7} ${path8} ${path9} ${path10} ${path10} ${path11} ${path11} ${path11} ${path11} ${path12} ${path15})
     moduleList=("healthcare" "tutorial" "tutorial" "tutorial" "appointment_mgt" "desktop_bff"
     "medical_record_mgt" "message_mgt" "mobile_bff" "notification_mgt" "sample_data_publisher"
     "company_data_service" "message_filtering" "pass_through" "auction_service" "stock_quote_data_backend"
     "stock_quote_summary_service" "airline_reservation" "car_rental" "hotel_reservation" "travel_agency" "restful_service"
-    "airline_reservation" "car_rental" "hotel_reservation" "hello_service")
+    "hello_service")
 
     echo ' _____         _
     |_   _|__  ___| |_ ___
